fe67231765e655d428ab2eae54f0608f50d74d87
Will2440 <will.caiger@gmail.com>
2026-09-18 17:06:53 +0200

    corrected qos format

--- simulations/data_collection/hpc/hpc_hyp_runscript.sh
+++ simulations/data_collection/hpc/hpc_hyp_runscript.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=general
+#SBATCH --qos=regular
 #SBATCH --cpus-per-task=1
-#SBATCH --account=caiger
 #SBATCH --array=1-1
 #SBATCH --time=01:00:00
 #SBATCH --mem=5G
